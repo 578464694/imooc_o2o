cdb4276e8935ec74e83c35dbc1f96a6e3c7df843
wangyu <578464694@qq.com>
2017-04-15 08:51:37 +0800

    finish the image upload

--- application/api/controller/Image.php
+++ application/api/controller/Image.php
@@ -1,7 +1,19 @@
 <?php
 namespace app\api\controller;
-
-class Image
+use think\Controller;
+use think\File;
+use think\Request;
+class Image extends Controller
 {
-
+    public function upload()
+    {
+        $file = Request::instance()->file('file');
+        // 给定一个目录
+        $info = $file->move('upload');  //public.upload
+        //　双传成功
+        if($info && $info->getPathname()) {
+            return show(1, 'success', '\\'.$info->getPathname());
+        }
+        return show(0,'upload error');
+    }
 }
